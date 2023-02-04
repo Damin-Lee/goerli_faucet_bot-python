a0e2d586aa289262c1c755b17b55ac8128510419
Damin-Lee <xavier.lee@krustuniverse.com>
2023-02-04 17:18:26 +0900

    chore: update script
    add env ADDRESS_1-4

--- index.py
+++ index.py
@@ -7,7 +7,12 @@ import os
 
 load_dotenv()
 
-ADDRESS = os.environ.get('ADDRESS')
+ADDRESS_1 = os.environ.get('ADDRESS_1')
+ADDRESS_2 = os.environ.get('ADDRESS_2')
+ADDRESS_3 = os.environ.get('ADDRESS_3')
+ADDRESS_4 = os.environ.get('ADDRESS_4')
+
+ADDRESS = [ADDRESS_1, ADDRESS_2, ADDRESS_3, ADDRESS_4]
 API_KEY = os.environ.get('2CAPTCHA_API_KEY')
 SITE_KEY = os.environ.get('SITE_KEY')
 
@@ -21,18 +26,18 @@ def get_captcha():
   return code
 
 def claim():
-  captcha = get_captcha()
-
   url = BASE_URL + "claim.php"
-  payload='address=' + ADDRESS+  '&captcha=' + captcha
-  headers = {
-    'Content-Type': 'application/x-www-form-urlencoded'
-  }
+  for address in ADDRESS:
+    captcha = get_captcha()
+    payload='address=' + address+  '&captcha=' + captcha
+    headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
 
-  try:
-    requests.request("POST", url, headers=headers, data=payload)
-  except Exception as e:
-    print('error: ' + e)
+    try:
+      requests.request("POST", url, headers=headers, data=payload)
+    except Exception as e:
+      print('error: ' + e)
 
 def main():
   ## claim every 24 hours (1440 minutes) + 5 minutes
@@ -42,5 +47,5 @@ def main():
     schedule.run_pending()
     time.sleep(1)
 
-if __name__ == '__main__':
+if name == '__main__':
   main()
